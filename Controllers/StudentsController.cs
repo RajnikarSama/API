c4ae7198f45a28e4050d76fc531b35f4babd6bf1
RajnikarSama <133262881+RajnikarSama@users.noreply.github.com>
2023-05-12 13:21:16 -0500

    Initial

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,71 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Xml.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private static List<Student> students = new List<Student>
+        {
+            new Student { Id = 1, Name = "John Doe", Age = 30, Courses = new string[2] { "dotnet", "Java" } },
+            new Student { Id = 2, Name = "Jane Smith", Age = 25, Courses = new string[5] { "dotnet", "Java", "Angular", "React", "SQL Server" } }
+        };
+        public StudentsController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> Get()
+        {
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Student> Get(int id)
+        {
+            var student = students.Find(p => p.Id == id);
+            if (student == null)
+                return NotFound();
+
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Student student)
+        {
+            student.Id = students.Count + 1;
+            students.Add(student);
+            return CreatedAtAction(nameof(Get), new { id = student.Id }, student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Student student)
+        {
+            var existingStudent = students.Find(p => p.Id == id);
+            if (existingStudent == null)
+                return NotFound();
+
+            existingStudent.Name = student.Name;
+            existingStudent.Age = student.Age;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var student = students.Find(p => p.Id == id);
+            if (student == null)
+                return NotFound();
+
+            students.Remove(student);
+            return NoContent();
+        }
+    }
+}
